e5e0eba88f8f73180255b80c22a7abbe8389ebd4
Jitendra  Kumar <JITENDra.KUMAR@NAVYUGINFO.COM>
2015-08-26 22:34:43 +0530

    test

--- test.rb
+++ test.rb
@@ -1,1 +1,1 @@
-puts %x[cd /home/ubuntu && tar -cvf game-of-life-in-python.tar.gz game-of-life-in-python]
+puts %x[cd /home/ubuntu && tar -cvf game-of-life-in-python.tar.gz game-of-life-in-python && mv game-of-life-in-python.tar.gz $CIRCLE_ARTIFACTS]
