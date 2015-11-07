0e53bd4fbb62d3d5e10fb388813d04855c98a50d
siddartha <siddartha.jangid@navyuginfo.com>
2015-11-07 17:40:21 +0530

    add fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,20 @@
 from fabric.api import local, run, env, put
 import os, time
-import urllib2 
+import urllib, urllib2, json
+import sys
+
+for arg in sys.argv:
+    print arg
+
+env.port = 22
+env.use_ssh_config = False
 # remote ssh credentials
-env.hosts = ['10.1.1.25']
-env.user = 'deploy'
-env.password = 'XXXXXXXX' #ssh password for user
+env.host_string = 'navyug@192.168.3.223'
+env.user = 'navyug'
+env.password = 'new life' #ssh password for user
 # or, specify path to server public key here:
-# env.key_filename = ''
- 
+#env.key_filename="/home/jigserv/.ssh/id_rsa.pub"
+env.no_keys = True 
 # specify path to files being deployed
 env.archive_source = '.'
  
@@ -15,43 +22,66 @@ env.archive_source = '.'
 env.archive_name = 'release'
  
 # specify path to deploy root dir - you need to create this
-env.deploy_project_root = '/var/www/projectx/'
+env.deploy_project_root = '/home/navyug/'
  
 # specify name of dir that will hold all deployed code
 env.deploy_release_dir = 'releases'
  
 # symlink name. Full path to deployed code is env.deploy_project_root + this
 env.deploy_current_dir = 'current'
+
+TOKEN="0b3fad910b1e253f8c624163e51873bdcb35744a"
+user = "TheExorcist"
+project = "game-of-life-in-python"
+
+
+[ {
+  "path" : "/tmp/circle-artifacts.8tQFahZ/game-of-life-in-python.tar.gz",
+  "pretty_path" : "$CIRCLE_ARTIFACTS/game-of-life-in-python.tar.gz",
+  "node_index" : 0,
+  "url" : "https://circle-artifacts.com/gh/TheExorcist/game-of-life-in-python/27/artifacts/0/tmp/circle-artifacts.8tQFahZ/game-of-life-in-python.tar.gz"
+} ]
  
 def update_local_copy():
-    # get latest / desired tag from your version control system
+    #get latest / desired tag from your version control system
+
+    run("whoami")
     print('updating local copy...')
-	content=urllib2.urlopen("https://circleci.com/api/v1/project/TheExorcist/game-of-life-in-python/?circle-token=artifact&limit=1&offset=0&filter=success").read()
-	resp = map(lambda x: x.strip(),  content.split(','))
-	num=(r for r in resp if r.split()[0]==':build_num').next()
-	
-	
- 
-def upload_archive():
-    # create archive from env.archive_source
-    print('creating archive...')
-    local('cd %s && zip -qr %s.zip -x=fabfile.py -x=fabfile.pyc *' \
-        % (env.archive_source, env.archive_name))
- 	
-	
-    # create time named dir in deploy dir
+    request = urllib2.Request('https://circleci.com/api/v1/project/TheExorcist/game-of-life-in-python/27/artifacts?\
+    circle-token=0b3fad910b1e253f8c624163e51873bdcb35744a', headers={"accept": "application/json"})
+    response = urllib2.urlopen(request) 
+    response_data = json.load(response)
+    
+
+
+def upload_archive(build_num=0):
+
+    print('updating local copy...')
+
+    request = urllib2.Request('https://circleci.com/api/v1/project/TheExorcist/game-of-life-in-python/{0}/artifacts?\
+    circle-token=0b3fad910b1e253f8c624163e51873bdcb35744a'.format(build_num), headers={"accept": "application/json"})
+
+    response = urllib2.urlopen(request) 
+    response_data = json.load(response)
+    artifact= urllib2.urlopen(response_data[0]['url']).read()
+    f=open('artifact.tar.gz', 'w')
+    f.write(artifact)
+
+    #create time named dir in deploy dir
     print('uploading archive...')
     deploy_timestring = time.strftime("%Y%m%d%H%M%S")
+    run('mkdir -p /home/navyug/releases')
     run('cd %s && mkdir %s' % (env.deploy_project_root + \
         env.deploy_release_dir, deploy_timestring))
  
-    # extract code into dir
+    #extract code into dir
     print('extracting code...')
     env.deploy_full_path = env.deploy_project_root + \
         env.deploy_release_dir + '/' + deploy_timestring
-    put(env.archive_name+'.zip', env.deploy_full_path)
-    run('cd %s && unzip -q %s.zip -d . && rm %s.zip' \
-        % (env.deploy_full_path, env.archive_name, env.archive_name))
+    print "uploading"
+    put("artifact.tar.gz", env.deploy_full_path)
+    #run('cd %s && unzip -q %s.zip -d . && rm %s.zip' \
+    #    % (env.deploy_full_path, env.archive_name, env.archive_name))
  
 def before_symlink():
     # code is uploaded, but not live. Perform final pre-deploy tasks here
@@ -75,10 +105,12 @@ def cleanup():
  
 def deploy():
     update_local_copy()
+    '''
     upload_archive()
     before_symlink()
     make_symlink()
     after_symlink()
     cleanup()
+    '''
     print('deploy complete!')
 
